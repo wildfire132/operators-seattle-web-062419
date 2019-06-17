278f975cf506a3f74cabe118e257a095ddf8fd51
Katana Tran <tkatyk21@gmail.com>
2019-06-17 04:36:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    puts unsafe
+  else
+    puts safe
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? "unsafe" : "safe"
 end
 	
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require_relative '../lib/operations.rb'
+
 
 RSpec.configure do |config|
-  # config here
+ require_relative '../lib/operations.rb'
 end
